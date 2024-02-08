edf4318b527b782433495644e84d9ba2ce91318a
neburpoots <neburpoots@hotmail.com>
2024-02-08 11:49:57 +0100

    added start to Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
 })
 export class AppComponent {
-  title = 'new-cosmos-client-application';
+  title = "angular-dashboard-page";
 }
